ead123d693256ed87a9bf3a2a1da4b009ddf57fc
zhao-huo-long <lijiuyi1995@outlook.com>
2023-07-05 23:19:50 +0800

    chore

--- scripts/postbuild.js
+++
@@ -1,7 +0,0 @@
-// const shell = require('shelljs');
-
-// shell.exec('npm link');
-
-// shell.exec('npm link tiny-time-js');
-
-// shell.exit();
--- scripts/prebuild.js
+++ scripts/prebuild.js
@@ -1,13 +1,17 @@
-const { rmSync } = require("fs");
+const { rmSync } = require("node:fs");
 
 [
   "./dist",
   "./plugins",
   "./esm",
   "umd",
+  "constants.d.ts",
+  "index.d.ts",
+  "index.js"
 ].map(dir => {
   try {
-    rmSync(dir, { recursive: true, })
+    rmSync(dir, { recursive: true, force: true })
   } catch (error) {
+    console.log(error)
   }
 });
